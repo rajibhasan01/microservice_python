de570cb7876844a8d7e3fcaee03991549686f11c
rajibhasan01 <hasan.rajib1996@gmail.com>
2023-05-05 21:14:23 +0600

    jwt decoded function and validate route added

--- server.py
+++ server.py
@@ -40,6 +40,25 @@ def login():
         return "invalid credentials", 401
 
 
+@server.route("/validate", method=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+
+    if not encoded_jwt:
+        return "missing credentials", 401
+
+    encoded_jwt = encoded_jwt.split(" ")[1]
+
+    try:
+        decoded = jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithm=["HS256"]
+        )
+    except:
+        return "not authorized", 403
+
+    return decoded, 200
+
+
 def createJWT(username, secret, authz):
     return jwt.encode(
        {
