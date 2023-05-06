6913e802c312fbfd3be408509c3e04e6de3a33f2
rajibhasan01 <hasan.rajib1996@gmail.com>
2023-05-06 11:41:45 +0600

    manifest file added

--- server.py
+++ server.py
@@ -40,7 +40,7 @@ def login():
         return "invalid credentials", 401
 
 
-@server.route("/validate", method=["POST"])
+@server.route("/validate", methods=["POST"])
 def validate():
     encoded_jwt = request.headers["Authorization"]
 
